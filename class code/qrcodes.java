3156add270cbae7ec3823bb16875d36a864f6c2b
Angelos Tsouvalas <50584010+AngelosTs@users.noreply.github.com>
2020-05-16 13:03:03 +0300

    Update qrcodes.java

--- class code/qrcodes.java
+++ class code/qrcodes.java
@@ -1,11 +1,11 @@
 class qrcode extends car{
-	private int qrcode
+	private int qrcode;
 	
 	public boolean check_qr(int scanned_qr){
 		if (this.qrcode == scanned_qr){
-			return false;
-		else{
 			return true;
+		else{
+			return false;
 			}
 		}
 	}
