2493882e003d300bebb4ce5f984da882629da136
Elias Trimintzios <39220817+GvasTheGreek@users.noreply.github.com>
2020-05-16 18:36:05 +0300

    Update qrcodes.java

--- class code/qrcodes.java
+++ class code/qrcodes.java
@@ -3,9 +3,10 @@ class qrcode extends car{  //  qr code is a subclass of class car
 	
 	public boolean check_qr(int scanned_qr){    //  check whether the qr code is the right one or not
 		if (this.qrcode == scanned_qr){
+			System.out.println("Successful scan");
 			return true;
 		else{
-			return false;
+			load_QR_screen();
 			}
 		}
 	}
@@ -13,7 +14,7 @@ class qrcode extends car{  //  qr code is a subclass of class car
 	public void load_QR_screen(){      //  and calls the qr code check
         	System.out.println("Displaying QR screen");
         	System.out.println("Please scan the QR-code");
-        	this.check_QR();
+        	this.check_qr(int scanned_qr);
     }
 	
 }
