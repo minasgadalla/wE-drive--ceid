73db443f62d3e49bff822b2471a9f4b5fe22e26e
Elias Trimintzios <39220817+GvasTheGreek@users.noreply.github.com>
2020-05-16 13:18:57 +0300

    Update Charger_Stations.java

--- class code/Charger_Stations.java
+++ class code/Charger_Stations.java
@@ -11,4 +11,17 @@ public class Charger_Stations extends Charger
         this.station_location = station_location;
         this.station_status = station_status;
     }  
+    
+     public void greeting()
+    {
+        System.out.println("Hello Everybody!");        
+    }
+    
+    public void creator(String name,String surname,int id,int year)
+    {
+        this.name=name;
+        this.surname=surname;
+        this.id=id;
+        this.year=year;
+    }
 }
