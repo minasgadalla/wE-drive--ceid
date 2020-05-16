eb7705ed09b82a8cb2c0965498479729dac21e20
Elias Trimintzios <39220817+GvasTheGreek@users.noreply.github.com>
2020-05-16 13:17:52 +0300

    Create Charger_Stations.java

---
+++ class code/Charger_Stations.java
@@ -0,0 +1,14 @@
+public class Charger_Stations extends Charger 
+{
+    //Some variables from "Charger" can be deleted , i'll update it later! 
+    public int station_id;
+    public int station_location;
+    public String station_status;
+    
+    public void station(int station_id, int station_location, String station_status)
+    {
+        this.station_id = station_id;
+        this.station_location = station_location;
+        this.station_status = station_status;
+    }  
+}
