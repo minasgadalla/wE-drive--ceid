a89cbee21706e90f881ac568a2b8fa6b27a21b6f
Elias Trimintzios <39220817+GvasTheGreek@users.noreply.github.com>
2020-05-22 23:09:47 +0300

    Update Volunteer.java

--- class code/Volunteer.java
+++ class code/Volunteer.java
@@ -1,7 +1,13 @@
 public class Volunteer extends User
 {
     public int user_id;
-    public int location;    
+    public int location;  
+    
+    public int update_loc(location)
+    {
+        public int new_location = location;
+        return new_location;
+    }
 }
 
 //Dont delete anything!
