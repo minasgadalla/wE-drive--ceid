ccf2c69adae4b549bdcaf122f3cb110cc8481d6b
Elias Trimintzios <39220817+GvasTheGreek@users.noreply.github.com>
2020-05-23 14:54:12 +0300

    Update Volunteer.java

--- class code/Volunteer.java
+++ class code/Volunteer.java
@@ -13,8 +13,9 @@ public class Volunteer extends User
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        }
         */
-    }
+   
     }
     
     
