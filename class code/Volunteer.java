8b4109d39d77dd49216763d5b9bbc3dfe21c381b
Elias Trimintzios <39220817+GvasTheGreek@users.noreply.github.com>
2020-05-23 10:53:16 +0300

    Update Volunteer.java

--- class code/Volunteer.java
+++ class code/Volunteer.java
@@ -3,6 +3,11 @@ public class Volunteer extends User
     public int user_id;
     public int location;  
     
+    public void loc_con(int user_id,int location)
+    {
+        user_id = this.user_id;
+        location = thislocation;
+    
     public int update_loc(location)
     {
         public int new_location = location;
