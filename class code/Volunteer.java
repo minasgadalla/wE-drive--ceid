1f22aa50a3e169ea8798cd3e2d9186c41a899518
TassiosVlasis <49943456+TassiosVlasis@users.noreply.github.com>
2020-05-25 22:37:01 +0300

    Update Volunteer.java
    Εσβησα τη μια συναρτηση

--- class code/Volunteer.java
+++ class code/Volunteer.java
@@ -192,11 +192,7 @@ public class Volunteer extends User
     }
     
   //These two functions need to be relocated!!.... The commiter can take care of it! (194 -- 206).
-    public void loc_con(int user_id,int location)
-    {
-        user_id = this.user_id;
-        location = this.location;
-    }
+    
     
     public int update_loc(location)
     {
