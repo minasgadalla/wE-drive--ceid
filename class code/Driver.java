b77dd8ccae130591a9d1a7d0717998dbdb9b14eb
Elias Trimintzios <39220817+GvasTheGreek@users.noreply.github.com>
2020-05-27 22:55:03 +0300

    Update Driver.java

--- class code/Driver.java
+++ class code/Driver.java
@@ -17,3 +17,27 @@ public class Driver extends User {
     public boolean status;
     public boolean card_return;
     public boolean we_Points_return;
+
+    public Driver[] car_info = new Driver[10];
+    
+    payment_details myDetail = new payment_details();
+    wE-points myPoints = new wE-points();
+    Car_Request request = new Car_Request();
+    QR_codes QR_CODE = new QR_codes();
+    
+    
+    public void select_car()
+    {
+        Scanner my_Object = new Scanner(System.in);
+        System.out.println("Click Next To Display available Cars");
+        String Next_Btn = my_Object.nextLine();
+        if (Next_Btn.equals("Next"))
+        {
+            display_cars();
+        }
+        else
+        {
+            select_car();
+        } 
+    }
+    
