2d7eb471a6656d272f43912e2fbaecdbf7c36ea6
Elias Trimintzios <39220817+GvasTheGreek@users.noreply.github.com>
2020-05-27 22:54:10 +0300

    Update Driver.java

--- class code/Driver.java
+++ class code/Driver.java
@@ -1,1 +1,26 @@
-//Driver Class
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareenginnering;
+
+import java.util.Scanner;
+import android.*;
+import java.time.*;
+
+/**
+ *
+ * @author user
+ */
+public class Driver extends SoftwareEnginnering {
+    
+    public int start_time;
+    public int end_time;
+    public int Total_time;
+    public float initial_battery_percentage;
+    public float final_battery_percentage;
+    public String value;
+    public boolean status;
+    public boolean card_return;
+    public boolean we_Points_return;
