58aa19e54da6cb7c8c88cdd734e6c3d94db65027
Angelos Tsouvalas <50584010+AngelosTs@users.noreply.github.com>
2020-05-28 17:19:20 +0300

    updated user const

--- class code/user.java
+++ class code/user.java
@@ -11,7 +11,21 @@ public class User {
  User() {
   System.out.println("Lets start creating your account!");
  }
+ 
+ User(boolean create){
+     if(!create){
+        this.pass = "TestUser";
+        this.p_num = "0";
+        this.email = "test@weride.com";
+        this.fname = "Test User";
+        this.drive_id = "1234";
+     }
+    
+ }
 
+ public String getfname(){
+    return this.fname;
+    }
 
  public boolean set_email(String i_email) {
   String regex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
@@ -39,12 +53,13 @@ public class User {
  }
 
  public boolean set_name(String name) {
-  if (!name.isEmpty()) {
-   this.fname = name;
-   return true;
-  } else {
-   return false;
-  }
+    if (!name.isEmpty()){
+    this.fname = name;
+    return true;
+}
+    else{
+    return false;
+    }
  }
 
  public boolean set_id_drive(String id) {
@@ -65,6 +80,6 @@ public class User {
    return false;
   }
  }
-
-
+ 
+ 
 }
