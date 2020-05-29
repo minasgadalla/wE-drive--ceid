32c9e5bc3b3e38dc3b33898c5abbc5f10c308b0a
christopher-cell <65423657+christopher-cell@users.noreply.github.com>
2020-05-29 16:26:22 +0300

    updated the class

--- class code/car.java
+++ class code/car.java
@@ -11,50 +11,50 @@ public class car {
  public boolean lock; // states whether  the car is locked or  not
 
  car(int idd, int bat) {
-    this.id = idd;
-    this.battery_pre = bat;
-    this.cordinates = "10:10:10";
-    this.last_service = "1/1/2020";
-    this.lock = false;
-    
-    }
-    
-    public int getid(){
-    return this.id;
-}
-public int getbat(){
-return this.battery_pre;
-}
+  this.id = idd;
+  this.battery_pre = bat;
+  this.cordinates = "10:10:10";
+  this.last_service = "1/1/2020";
+  this.lock = false;
 
-public void startDrive(){
-    Scanner in2 = new Scanner(System.in);
-     System.out.println("--------------------");
-     System.out.println("--------------------");
-     System.out.println("Tsouf tsouf. You are driving:");
-     System.out.println("   Car ID: " + this.getid());
-     System.out.println("   Battery Precentage: " + this.getbat()+"%");
-     System.out.println("");
-     boolean quit = false;
-     while(!quit){
-     System.out.println("Options:");
-     System.out.println("   1.Terminate ride");
-     System.out.println("   2.Pause ride");
+ }
 
-     int menuItem = in2.nextInt();
+ public int getid() {
+  return this.id;
+ }
+ public int getbat() {
+  return this.battery_pre;
+ }
+
+ public void startDrive() {
+  Scanner in2 = new Scanner(System.in);
+  System.out.println("--------------------");
+  System.out.println("--------------------");
+  System.out.println("Tsouf tsouf. You are driving:");
+  System.out.println("   Car ID: " + this.getid());
+  System.out.println("   Battery Precentage: " + this.getbat() + "%");
+  System.out.println("");
+  boolean quit = false;
+  while (!quit) {
+   System.out.println("Options:");
+   System.out.println("   1.Terminate ride");
+   System.out.println("   2.Pause ride");
+
+   int menuItem = in2.nextInt();
    switch (menuItem) {
     case 1:
      System.out.println("You've chosen to Terminate");
-     
+
     case 2:
      System.out.println("You've chosen to Pause the ride");
 
     default:
      System.out.println("Invalid choice.");
-    }
-}
-}
-    
- 
+   }
+  }
+ }
+
+
  public boolean valid_parking_zone(String cord) {
   if (cord != null && !cord.isEmpty()) {
    return true;
@@ -62,14 +62,14 @@ public void startDrive(){
    return false;
   }
  }
- 
+
 
  public boolean car_battery() {
   if (this.battery_pre < 20) {
-    return true;
-   } else {
-    return false;
-   }
+   return true;
+  } else {
+   return false;
   }
-
  }
+
+}
