a858c67b6ef748cd0c3f08d1a4adb4d4bcc8cb26
minasgadalla <minasgadalla@gmail.com>
2020-05-29 18:45:19 +0300

    payment class

---
+++ class code/payreq.java
@@ -0,0 +1,29 @@
+
+public class payreq
+{
+    private String pay_date;
+    private String req_date;
+    private int pay_cost;
+    private boolean paid;
+     
+    /**
+     * Constructor for objects of class payreq
+     */
+    public payreq( String date, int cost)
+    {
+        this.pay_date =  date;
+        this.pay_cost = cost;
+    }
+
+    public void make_payment(){
+        this.paid = true;
+    }
+    public boolean pay_done()
+    {
+        if(this.paid){
+            return true;
+        }else{
+        return false;
+    }
+    }
+}
