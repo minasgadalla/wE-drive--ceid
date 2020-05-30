63117a58a6b545c7c803539189d7c9b1bb11cb07
christopher-cell <65423657+christopher-cell@users.noreply.github.com>
2020-05-30 15:36:58 +0300

    Update userscards.java

--- class code/userscards.java
+++ class code/userscards.java
@@ -1,47 +1,44 @@
+public class userscards {
+ // instance variables - replace the example below with your own
+ private int card_number;
+ private String card_name;
+ private int card_cvc;
+ private String card_exp;
+ private boolean card_default;
 
-public class userscards
-{
-    // instance variables - replace the example below with your own
-    private int card_number;
-    private String card_name;
-    private int card_cvc;
-    private String card_exp;
-    private boolean card_default;
+ /**
+  * Constructor for objects of class usercards
+  */
+ public userscards() {
+  // initialise instance variables
 
-    /**
-     * Constructor for objects of class usercards
-     */
-    public userscards()
-    {
-        // initialise instance variables
+ }
 
-    }
-    
-    public void printCard(){
-        System.out.println("Card Number " +this.card_default);
-        System.out.println(" Name " + this.card_name);
-    }
-    
-    public void add_new_card(int num, String name, int cvc, String exp, boolean def){
-            this.card_number = num;
-        this.card_name = name;
-        this.card_cvc =  cvc;
-        this. card_exp = exp;
-        this.card_default = def;
-    }
-    
-    
-    public int get_card_info(){
-        return this.card_number;
-        
-    }
-    
-    public boolean is_default(){
-        if(this.card_default){
-            return true;
-        }else{
-           return false;
-        }
-    }
-    
+ public void printCard() {
+  System.out.println("Card Number " + this.card_default);
+  System.out.println(" Name " + this.card_name);
+ }
+
+ public void add_new_card(int num, String name, int cvc, String exp, boolean def) {
+  this.card_number = num;
+  this.card_name = name;
+  this.card_cvc = cvc;
+  this.card_exp = exp;
+  this.card_default = def;
+ }
+
+
+ public int get_card_info() {
+  return this.card_number;
+
+ }
+
+ public boolean is_default() {
+  if (this.card_default) {
+   return true;
+  } else {
+   return false;
+  }
+ }
+
 }
