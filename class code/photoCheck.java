3b0143aeb92403957447cf1733954374958f5ec2
christopher-cell <65423657+christopher-cell@users.noreply.github.com>
2020-05-30 18:00:35 +0300

    adding the class for photo checking

---
+++ class code/photoCheck.java
@@ -0,0 +1,38 @@
+
+public class photoCheck
+{
+    // instance variables - replace the example below with your own
+
+    public photoCheck()
+    {
+
+    }
+
+    public void connction()
+    {
+          System.out.println("Please wait...");
+
+    }
+    
+    public boolean upload_photos(String photo_upload){
+        
+        if (photo_upload != null && !photo_upload.isEmpty()) {
+            evaluation(photo_upload);
+            return true;
+            
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean evaluation(String photo){
+        if(true){
+            // check photo condition
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+    
+}
