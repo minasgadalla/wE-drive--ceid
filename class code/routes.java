6469359ce2a0518463d34f654fe9e613c4f828d2
Angelos Tsouvalas <50584010+AngelosTs@users.noreply.github.com>
2020-05-30 18:06:01 +0300

    codeb and update

--- class code/routes.java
+++ class code/routes.java
@@ -1,21 +1,34 @@
-public class routes
-{
-    private int type;
-    private int route_time;
-    private int route_cost;
-    public routes(int t)
-    {
-        // initialise instance variables
-        this.type = t;
-        this.route_time = -1;
-        this.route_cost = 0;
-    }
-    
-    public void save_route_time(int time)
-    {
-        this.route_time = time;
-    }
-    public void save_route_cost(int cost){
-        this.route_cost = cost;
-    }
+public class routes {
+ private int type;
+ private int route_time;
+ private int route_cost;
+
+ public routes() {
+  // initialise instance variables
+  this.route_time = 18;
+  this.route_cost = 0;
+ }
+
+ public void set_type(int t) {
+  this.type = t;
+
+ }
+
+ public void save_route_time(int time) {
+  this.route_time = time;
+ }
+ public void save_route_cost(int cost) {
+  this.route_cost = cost;
+ }
+
+ public int get_routes() {
+  return route_cost;
+ }
+
+ public int calculate_cost(int time) {
+  this.route_cost = (int)(time * 0.20 + 1) + 1;
+  return this.route_cost;
+ }
+
+
 }
