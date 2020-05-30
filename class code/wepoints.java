58511017e143d486373f330dda6aaf92061f5d40
Angelos Tsouvalas <50584010+AngelosTs@users.noreply.github.com>
2020-05-30 15:38:28 +0300

    wepoints a change

--- class code/wepoints.java
+++ class code/wepoints.java
@@ -1,27 +1,27 @@
+public class wepoints {
+ private int wepoints;
 
-public class wepoints
-{
-    private int wepoints;
-    
-    public wepoints()
-    {
-        this.wepoints = 0;
-    }
-    
-    public int get_wepoints(int y)
-    {
-        // put your code here
-        return this.wepoints;
-    }
-    
-    public void sub_wepoints(int sub){
-        this.wepoints = this.wepoints - sub;    
-    }
-    
-    public void zero_wepoints(){
-        this.wepoints = 0;
-    
-    }
-    
-    
+ public wepoints() {
+  this.wepoints = 150;
+ }
+
+ public int get_wepoints() {
+  // put your code here
+  return this.wepoints;
+ }
+
+ public void sub_wepoints(int sub) {
+  this.wepoints = this.wepoints - sub;
+ }
+
+ public void calc_wepoints(int time) {
+  this.wepoints = this.wepoints + time * 5;
+ }
+
+ public void zero_wepoints() {
+  this.wepoints = 0;
+
+ }
+
+
 }
