a4ee2e403f816acac852bda0e2ab5508c07da7b9
Angelos Tsouvalas <50584010+AngelosTs@users.noreply.github.com>
2020-05-30 18:03:59 +0300

    codeb and update

--- class code/qrcodes.java
+++ class code/qrcodes.java
@@ -1,20 +1,28 @@
-class qrcode extends car { //  qr code is a subclass of class car
+public class qrcode { //  qr code is a subclass of class car
+
  private int qrcode;
+
+ public void load_QR_screen() { //  and calls the qr code check
+  System.out.println("Displaying QR screen");
+  System.out.println("Please scan the QR-code");
+ }
 
  public boolean check_qr(int scanned_qr) { //  check whether the qr code is the right one or not
   if (this.qrcode == scanned_qr) {
    System.out.println("Successful scan");
    return true;
-   else {
-    load_QR_screen();
-   }
+  } else {
+   load_QR_screen();
   }
+  return false;
  }
 
- public void load_QR_screen() { //  and calls the qr code check
-  System.out.println("Displaying QR screen");
-  System.out.println("Please scan the QR-code");
-  this.check_qr(int scanned_qr);
- }
+ public boolean scan(int qr) {
+  if (this.qrcode == qr) {
+   return true;
+  } else {
+   return false;
+  }
 
+ }
 }
